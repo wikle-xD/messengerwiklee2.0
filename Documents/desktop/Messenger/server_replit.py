fd4f9b1343ca13f1eddd07bda84d0e922de2eeca
wikle <xuturuteamxtr@gmail.com>
2025-11-22 13:21:41 +0200

    Delete Documents/desktop/Messenger/server_replit.py

--- Documents/desktop/Messenger/server_replit.py
+++
@@ -1,9 +0,0 @@
-import os
-import uvicorn
-
-# Import FastAPI app from server.py
-from server import app
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", "8000"))
-    uvicorn.run(app, host="0.0.0.0", port=port)
